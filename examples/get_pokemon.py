becaab30baec394122a528c958201af1b3ece981
Kalashnyk Artem <kalart@ukr.net>
2023-06-30 19:30:52 +0300

    Data from lessons 18-19

--- examples/get_pokemon.py
+++ examples/get_pokemon.py
@@ -1,14 +1,10 @@
 import json
 from dataclasses import asdict, dataclass
-from random import choice, randint
-from string import ascii_letters
 
 import requests
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
-from core.models import User
 
 
 def filter_by_keys(source: dict, keys: list[str]) -> dict:
@@ -113,48 +109,4 @@ def get_all_pokemons(request):
     return HttpResponse(
         content_type="application/json",
         content=json.dumps(all_pokemons),
-    )
-
-
-# *****************************************************
-# All roles are hardcoded instead of beeing used in the database
-# *****************************************************
-
-ROLES = {
-    "ADMIN": 1,
-    "MANAGER": 2,
-    "USER": 3,
-}
-
-
-def _get_random_string(size: int = 5) -> str:
-    return "".join([choice(ascii_letters) for _ in range(size)])
-
-
-def create_random_user(request):
-    email_prefix = _get_random_string(size=randint(5, 8))
-    email_affix = _get_random_string(size=randint(2, 5))
-    email = "".join((email_prefix, "@", email_affix, ".com"))
-
-    user = User.objects.create(
-        username=_get_random_string(size=randint(5, 10)),
-        email=email,
-        first_name=_get_random_string(size=randint(5, 10)),
-        last_name=_get_random_string(size=randint(5, 10)),
-        password=_get_random_string(size=randint(10, 20)),
-        role=ROLES["USER"],
-    )
-
-    result = {
-        "id": user.pk,
-        "username": user.username,
-        "email": user.email,
-        "firstName": user.first_name,
-        "lastName": user.last_name,
-        "role": user.role,
-    }
-
-    return HttpResponse(
-        content_type="application/json",
-        content=json.dumps(result),
     )
