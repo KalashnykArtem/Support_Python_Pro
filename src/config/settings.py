374efc543a78992ce8fefc0bfcb0cbc73a8703e7
Kalashnyk Artem <kalart@ukr.net>
2023-07-06 09:56:43 +0300

    data from lesson 22

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+SRC_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = SRC_DIR.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -46,7 +47,11 @@ THIRD_PARTY_APPS = [
     "rest_framework_simplejwt",
 ]
 
-LOCAL_APPS = ["core", "authentication"]
+LOCAL_APPS = [
+    "authentication",
+    "users",
+    "tickets",
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
@@ -86,7 +91,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": ROOT_DIR / "db.sqlite3",
     }
 }
 
@@ -136,7 +141,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
 
 
-AUTH_USER_MODEL = "core.User"
+AUTH_USER_MODEL = "users.User"
 
 
 REST_FRAMEWORK = {
